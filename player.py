12e9a44864d4df07da7f0b8e60b362929e2fa916
Matt <henriktheberg@gmail.com>
2022-02-11 22:44:52 +0100

    Player variables payload and goal, and functions to update

--- player.py
+++ player.py
@@ -8,3 +8,25 @@ class Player:
       self.rect = rect
       self.action = [0, 0]
       self.speed = speed
+      self.payload = False
+      self.goal = False
+
+  def load_payload(self):
+    if self.payload:
+      return False
+    else:
+      self.payload = True
+      return True
+    
+
+  def enterGoal(self):
+    if self.payload:
+      self.goal = True
+    else:
+      return False
+  
+  def reset(self, x, y):
+    self.payload = False
+    self.goal = False
+    self.rect.x = x
+    self.rect.y = y
